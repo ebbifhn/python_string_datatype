4bbed71bb447daee6a338cdf1d35973a625f7d08
ebbifhn <fhnebbi@gmail.com>
2022-09-27 19:04:09 -0600

    end of class tuesday

--- python_strings.py
+++ python_strings.py
@@ -1,35 +1,45 @@
+
 # TODO Create Variables
 #   - Create the following variables
 #   - my_first_name
 #       -set this equal to your first name
+my_first_name = 'Scott'
 #   - my_last_name
 #       -set this equal to your last name
+my_last_name = 'Hadzik'
 #   - my_year_of_birth
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
+my_birth_year = 1980
 #   - current_year
-#       -set this equal to 2020
+#       -set this equal to 2022
+current_year = 2022
 
 
 
 
 # TODO String Indexing
 #   - Print the following items (one per line) (print using variables)
-#       - first name  
+#       - first name
 #       - last name
 #       - first letter of your first name (use the +index)
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - second two letter of your last name (use the -index)
-
-
+print (my_first_name)
+print (my_last_name)
+print(my_first_name[0])
+print(my_last_name[-5])
+print(my_first_name[0:2])
+print(my_last_name[-2:])
 
 
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
-
-
+print(my_first_name + my_last_name)
+print(my_first_name,my_last_name)
+print((my_first_name,"\n") * 6)
 
 
 
